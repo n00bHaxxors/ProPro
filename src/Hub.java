9e215772f3f43f020b6df39f89a4ef4a4117ea01
Klanc <clank201@gmail.com>
2017-04-30 16:03:53 +0200

    Afegides algunes classes principals

---
+++ src/Hub.java
@@ -0,0 +1,20 @@
+/** @file Hub.java
+ @brief Classe Hub
+ */
+import java.util.ArrayList;
+
+/** @class Hub
+ @brief Hub
+ @author Lluís Trilla
+ */
+
+public class Hub {asdasdsaadsdasads
+    private ArrayList<MT_Indirecte>
+
+    /**
+     * @brief Crea un Hub
+     * @pre cert
+     * @post Crea un hub buit
+     */
+    public Hub(){}
+}
