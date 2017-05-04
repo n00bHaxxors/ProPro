b64fabfacc3e7c38b91f834c4a0dcba27b02a50a
misterchef102 <misterchef102@users.noreply.github.com>
2017-05-04 13:36:19 +0200

    Creats constructors de mt directe

--- src/MT_Directe.java
+++ src/MT_Directe.java
@@ -3,19 +3,31 @@
  @author Marc Cané Salamià
  */
 
+//1-portar a un lloc amb tots els seus fills (sense origen desti)
+
+//2-Conexio directe: té origen i desti (que son secundaris del mateix lloc)
+
 import java.time.*;
 
 public class MT_Directe extends Mitja_Transport {
     private int preu;
     private LocalTime durada;
+    private PuntInteres desti;
 
     /** @brief Constructor amb parametres
      @pre cert
-     @post Mitja de transport amb nom, preu i durada creat*/
-    public Mitja_Transport(String n, int p, LocalTime d){
+     @post Mitja de transport amb nom, preu, durada creat*/
+    public Mitja_Transport(String n, int p, LocalTime d, PuntInteres d){ //connexio directe
         nom=n; preu=p; durada=d;
     }
-
+ 
+    /** @brief Constructor amb parametres
+     @pre cert
+     @post Mitja de transport amb nom, preu i durada creat*/
+    public Mitja_Transport(String n, int p, LocalTime d){ //transport random
+        Mitja_Transport(n,p,d,NULL);    
+    }
+ 
     /** @brief Consulta el preu d'us del mitjà de trasport
      @pre cert
      @post Retorna el preu del mitja de transport*/
