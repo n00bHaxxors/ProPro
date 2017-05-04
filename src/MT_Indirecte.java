bd1750190122d8a9d073243532ffd3b2a942a793
sipwarriper <sipwarriper@gmail.com>
2017-05-05 00:14:31 +0200

    correccions codi MT_Indirecte

--- src/MT_Indirecte.java
+++ src/MT_Indirecte.java
@@ -25,27 +25,27 @@ public class MT_Indirecte extends Mitja_Transport {
     /** @brief Constructor amb paràmetres
      @pre h no buida i p i d tenen la mateixa mida que h
      @post Mitjà de transport indirecte amb horari, preus i duracio creat*/
-    public MT_Indirecte(String n, LocalDateTime h[], int p[], LocalTime d[]){
+    public MT_Indirecte(String n, Partença [] part){
         super(n);
-        horaris=h; preu=p; durada=d;
+        partences = part;
     }
 
     /** @brief Consultar horaris
      @pre cert
      @post Retorna els horaris del Mitjà de transport*/
-    public LocalDateTime[] horaris(){
+    /*public LocalDateTime[] horaris(){
         return horaris;
-    }
+    }*/
 
     /** @brief Consultar durada d'un mitjà
      @pre n>=0 i n<mida
      @post Retorna la durada del MT*/
-    public LocalTime durada(int n){
+    /*public LocalTime durada(int n){
         return durada[n];
-    }
+    }*/
 
     /** @brief Consulta el preu per utilitzar el mitjà de transport
      @pre n>=0 i n<mida
      @post Retorna el preu per l'ús del MT*/
-    public int preu(int n){ return preu[n]; }
+    //public int preu(int n){ return preu[n]; }
 }
