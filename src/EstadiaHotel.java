40bc28e8ea16f00c047f8ae23c4de0bdb6e2f440
sipwarriper <sipwarriper@gmail.com>
2017-05-10 15:38:16 +0200

    Pres i Posts

--- src/EstadiaHotel.java
+++ src/EstadiaHotel.java
@@ -10,10 +10,15 @@ import java.time.*;
 public class EstadiaHotel extends Activitat {
     private Allotjament hotel;
     
+    /** @brief Constructor de una Estadia a un Hotel
+     @pre cert
+     @post Crea una estadia amb les dades donades*/
     public EstadiaHotel(Allotjament h, MonthDay dia, LocalTime hora){
         super(dia, hora);
         hotel = h;
     }
-    
+    /** @brief Consulta el nom del Allotjament
+     @pre cert
+     @post Retorna el nom del Allotjament*/
     public String nomAllotjament(){ return hotel.nom(); }
 }
