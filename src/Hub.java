6defd6739d468e9e3c6df6baca54f14564829d00
misterchef102 <marc00077@hotmail.com>
2017-05-13 00:16:06 +0200

    Partença -> mt indirecte mt indirecte -> hub

--- src/Hub.java
+++ src/Hub.java
@@ -1,20 +1,44 @@
 /** @file Hub.java
  @brief Classe Hub
  */
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 /** @class Hub
  @brief Hub
- @author Lluís Trilla
+ @author Lluís Trilla ft. Markus
  */
 
 public class Hub {
-    private ArrayList<MT_Indirecte> Transports;
 
-    /**
-     * @brief Crea un Hub
-     * @pre cert
-     * @post Crea un hub buit
-     */
-    public Hub(){}
+    private LocalTime temps_trasllat_origen, temps_trasllat_desti;
+    private ArrayList<MT_Indirecte> transports; //Superclasse??
+    private Lloc desti;
+
+    /** @brief Constructor amb paràmetres
+     @pre WIP
+     @post Mitjà de transport indirecte amb ?? */
+    public Hub(LocalTime o, LocalTime d, Lloc de, ArrayList<MT_Indirecte> tr){
+        temps_trasllat_origen=o; temps_trasllat_desti=d; desti=de; transports = tr;
+    }
+
+    /** @brief Consultar horaris
+     @pre cert
+     @post Retorna els horaris del Mitjà de transport*/
+    /*public LocalDateTime[] horaris(){
+        return horaris;
+    }*/ //WIP
+
+    /** @brief Consultar durada d'un mitjà
+     @pre n>=0 i n<mida
+     @post Retorna la durada del MT*/
+    public LocalTime durada(int n){
+        return transports.get(n).durada;
+    }
+
+    /** @brief Consulta el preu per utilitzar el mitjà de transport
+     @pre n>=0 i n<mida
+     @post Retorna el preu per l'ús del MT*/
+    public int preu(int n){ return transports.get(n).preu; }
+
 }
--- src/MT_Indirecte.java
+++ src/MT_Indirecte.java
@@ -8,43 +8,14 @@ import java.util.ArrayList;
 
 public class MT_Indirecte extends Mitja_Transport {
 
-    public static class Partença{
-        public Partença(LocalDate h,LocalTime hora, LocalTime d, int p){
-            dia=h; hora=hora;durada=d; preu=p;
-        }
-        public LocalDate dia;
-        public LocalTime hora;
-        public LocalTime durada;
-        public int preu;
-    }
-    private LocalTime temps_trasllat_origen, temps_trasllat_desti;
-    private ArrayList<Partença> partences;
-    private Lloc desti;
+    public LocalDate dia;
+    public LocalTime hora;
+    public LocalTime durada;
+    public int preu; //hauria d'anar a la superclasse
 
-    /** @brief Constructor amb paràmetres
-     @pre WIP
-     @post Mitjà de transport indirecte amb ?? */
-    public MT_Indirecte(String n, LocalTime o, LocalTime d, Lloc de, ArrayList<Partença> part){
+    public MT_Indirecte (String n, LocalDate h, LocalTime hora, LocalTime d, int p){
         super(n);
-        temps_trasllat_origen=o; temps_trasllat_desti=d; desti=de; partences = part;
+        dia=h; hora=hora; durada=d; preu=p;
     }
 
-    /** @brief Consultar horaris
-     @pre cert
-     @post Retorna els horaris del Mitjà de transport*/
-    /*public LocalDateTime[] horaris(){
-        return horaris;
-    }*/ //WIP
-
-    /** @brief Consultar durada d'un mitjà
-     @pre n>=0 i n<mida
-     @post Retorna la durada del MT*/
-    public LocalTime durada(int n){
-        return partences.get(n).durada;
-    }
-
-    /** @brief Consulta el preu per utilitzar el mitjà de transport
-     @pre n>=0 i n<mida
-     @post Retorna el preu per l'ús del MT*/
-    public int preu(int n){ return partences.get(n).preu; }
 }
