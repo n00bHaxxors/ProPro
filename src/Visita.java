5ffdef4301d29138b9357f4e60be76038c747b81
sipwarriper <sipwarriper@gmail.com>
2017-05-13 12:04:37 +0200

    Creat metode abstracte a Activitat per calcular la satisfaccio total d'un grup de clients

--- src/Visita.java
+++ src/Visita.java
@@ -4,6 +4,7 @@
  */
 
 import java.time.*;
+import java.util.Iterator;
 
 public class Visita extends Activitat{
     private Visitable visitat;
@@ -39,5 +40,23 @@ public class Visita extends Activitat{
     @Override
     public LocalTime Duracio(){
         return visitat.tempsVisitaRec();
+    }
+    
+    /** @brief Calcula la Satisfaccio que afageix l'activitat
+     @pre cert
+     @post Retorna la satisfaccio calculada*/
+    @Override
+    public int Satisfaccio(GrupClients g){
+        Iterator<Client> itr = g.iteradorClients();
+        int resultat = 0;
+        while(itr.hasNext()){
+            Client c = itr.next();
+            Iterator<String> itr2= c.IteradorPreferencies();
+            while (itr2.hasNext()){
+                String preferencia = itr2.next();
+                if (visitat.conteCaracteristica(preferencia)) resultat++;
+            }
+        }
+        return resultat;
     }
 }
