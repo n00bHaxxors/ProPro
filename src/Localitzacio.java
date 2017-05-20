1fa727945ae4f481f5569910927755c366e1b3d7
sipwarriper <sipwarriper@gmail.com>
2017-05-20 16:06:49 +0200

    Pres i posts que he vist que faltaven

--- src/Localitzacio.java
+++ src/Localitzacio.java
@@ -11,10 +11,18 @@ import java.util.TimeZone;
 
 
 public abstract class Localitzacio {
-    
+    /** @brief Consulta el nom de la localització
+	@pre cert
+	@post retorna el nom */
     public abstract String nom();
     
+    /** @brief Consulta les coordenades de la localització
+	@pre cert
+	@post retorna les coordenades */
     public abstract Coordenada coordenada();
     
+    /** @brief Consulta la zona horària de la Localització
+	@pre cert
+	@post retorna la zona */
     public abstract TimeZone zona();
 }
